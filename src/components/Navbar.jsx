cc41a8a81200a85fa79a1f2dc5d6ff79ad024e63
yatinsingh2007 <kshtriyatinsingh@Kshtris-MacBook-Pro.local>
2025-03-18 12:32:11 +0530

    responsivness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import {motion} from 'framer-motion'
 const Navbar = () => {
   const [loader, setLoader] = useState(true);
   useEffect(() => {
@@ -11,20 +12,22 @@ const Navbar = () => {
   return (
     <div>
       {!loader && 
-      <header>
-      <nav className="bg-zinc-950 flex justify-between font-mono px-5 py-5 text-white pb-5">
-        <div className="primary-nav font-extrabold text-xl">
-          Portfolio
-        </div>
-        <div className="flex gap-5">
-          <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/home" className="group-hover:text-black px-2">Home</Link></div>
-          <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/" className="group-hover:text-black px-2">About</Link></div>
-          <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/skills" className="group-hover:text-black px-2">Skills</Link></div>
-          <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/" className="group-hover:text-black px-2">Projects</Link></div>
-          <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/contact" className="group-hover:text-black px-2">Contact Me</Link></div>
-        </div>
-      </nav>
-    </header>}
+      <motion.main initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:1,ease:"easeOut"}}>
+        <header>
+        <nav className="bg-zinc-950 flex justify-between font-mono px-5 py-5 text-white pb-5">
+          <div className="primary-nav font-extrabold text-xl hidden md:block">
+            Portfolio
+          </div>
+          <div className="flex gap-5">
+            <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/home" className="group-hover:text-black px-2">Home</Link></div>
+            <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/" className="group-hover:text-black px-2">About</Link></div>
+            <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/skills" className="group-hover:text-black px-2">Skills</Link></div>
+            <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/" className="group-hover:text-black px-2">Projects</Link></div>
+            <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/contact" className="group-hover:text-black px-2">Contact</Link></div>
+          </div>
+        </nav>
+      </header>
+      </motion.main>}
     </div>
   );
 }
