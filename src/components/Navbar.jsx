8cd85fcbeddc9f5513128fc0539890de2b0783db
yatinsingh2007 <kshtriyatinsingh@Kshtris-MacBook-Pro.local>
2025-03-31 20:10:11 +0530

    qejfb

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoader(false);
-    }, 2000);
+    }, 2350);
     return () => clearTimeout(timer);
   }, []);
   return (
@@ -14,7 +14,7 @@ const Navbar = () => {
       {!loader && 
       <motion.main initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:1,ease:"easeOut"}}>
         <header>
-        <nav className="bg-black flex justify-center font-mono py-5 pr-5 pl-56 text-white opacity-70 items-center lg:text-xl md:text-lg sm:px-5 sm:py-5 md:px-5 md:py-5">
+        <nav className="bg-black flex justify-center font-mono py-5 pr-5 pl-56 text-white opacity-70 items-center lg:text-xl md:text-lg sm:px-5 sm:py-5 md:px-5 md:py-5 lg:px-5">
           <div className="flex gap-20 md:gap-10 sm:pl-48">
             <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/home" className="group-hover:text-black px-2">Home</Link></div>
             <div className="p-2 hover:bg-white rounded-md hover:text-black"><Link to="/skills" className="group-hover:text-black px-2">Skills</Link></div>
