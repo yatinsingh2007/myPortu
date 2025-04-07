cf2812d48b87e46be379e91799e6f22fd5fe1167
yatinsingh2007 <kshtriyatinsingh@Kshtris-MacBook-Pro.local>
2025-04-07 10:50:12 +0530

    position issue fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,15 +5,15 @@ const Contact = () => {
   return (
     <>
         <motion.main initial={{x:-100,opacity:0}} animate={{x:0,opacity:1}} transition={{duration:1,ease:"easeOut"}}>
-            <div className="mt-32 flex flex-col justify-center lg:text-xl md:gap-64">
+            <div className="mt-32 flex flex-col justify-start lg:text-xl md:gap-64">
                 <div className="lg:ml-96 md:">
                     <div className="flex justify-start items-center">
-                        <h1 className="text-white font-extrabold text-4xl text-center p-10">Contact Me</h1>
+                        <h1 className="text-white font-mono text-4xl text-center p-10">Contact Me</h1>
                         <DotLottieReact src="https://lottie.host/faded867-64d2-4d4f-adfe-5b10476714fb/9A2nTN6BzD.lottie" loop autoplay className="w-52"/>
                     </div>
                     <div>
-                        <p className="text-white font-bold text-left p-4">Discuss a Project or just want to say Hi! My inbox is open for all.</p>
-                        <p className="text-white font-extrabold text-left p-6 text-2xl md:text-left">yatin4591@gmail.com</p>
+                        <p className="text-white font-mono text-left p-4">Discuss a Project or just want to say Hi! My inbox is open for all.</p>
+                        <p className="text-white font-mono text-left p-6 text-2xl md:text-left">yatin4591@gmail.com</p>
                         <div className="flex items-center justify-center p-4">
                         <div className="flex justify-start w-screen">
                             <div><a href="https://www.instagram.com/yatinsingh2007" target="_blank" rel="noopener noreferrer" className="text-pink-500 hover:text-pink-600 text-3xl">
--- src/components/HamburgerNav.jsx
+++ src/components/HamburgerNav.jsx
@@ -2,19 +2,22 @@ import {Menu , X} from 'lucide-react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 const HamburgerNav = () => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+      setOpen(false);
+    }
   return (
    <>
    {open ? <X className='text-white' onClick={() => setOpen(!open)} /> : <Menu className='text-white' onClick={() => setOpen(!open)} />}
     {open && (
         <>
-        <div className='absolute top-0 left-0 w-full h-screen bg-stone-800 flex flex-col justify-start items-start flex-wrap gap-10'>
+        <div className='absolute top-0 left-0 right-0 bottom-0 w-full h-screen bg-black flex flex-col justify-start items-start flex-wrap gap-10 z-20'>
            <X className='text-white' onClick={() => setOpen(!open)} />
            <div className='flex flex-col items-center justify-center flex-wrap gap-10 pl-32 pt-20'>
-            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg'><Link to='/home'>Home</Link></div>
-            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg'><Link to='/skills'>Skills</Link></div>
-            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg'><Link to='/projects'>Projects</Link></div>
-            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg'><Link to='/contact'>Contact</Link></div>
+            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg' onClick={handleClick}><Link to='/home'>Home</Link></div>
+            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg' onClick={handleClick}><Link to='/skills'>Skills</Link></div>
+            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg' onClick={handleClick}><Link to='/projects'>Projects</Link></div>
+            <div className='text-white font-mono hover:text-black hover:bg-white p-2 rounded-lg' onClick={handleClick}><Link to='/contact'>Contact</Link></div>
            </div>
         </div>
         </>
